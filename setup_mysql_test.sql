1f71843c938f64214ab322ded4bdb4fb0e662e9f
Mikenziokah <mikenziokah@gmail.com>
2023-11-20 11:05:34 -0800

    mysql test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- prepares a MySQL server test
+-- creating a database and a user with a password
+-- setting previlages for the user
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
