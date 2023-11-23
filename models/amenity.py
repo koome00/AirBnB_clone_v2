240ffe37cab8c34f7b1688c43891b9086ca1dbf4
koome <koomemc@gmail.com>
2023-11-23 03:35:04 +0300

    fix circular import

--- models/amenity.py
+++ models/amenity.py
@@ -4,6 +4,7 @@
 '''
 from os import getenv
 from models.base_model import BaseModel, Base
+from models.place import place_amenity
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -13,12 +14,9 @@ class Amenity(BaseModel, Base):
         Implementation for the Amenities.
     '''
     __tablename__ = "amenities"
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        from models.place import association_table
-        name = Column(String(128), nullable=False, default="")
-
-        place_amenities = relationship("Place", secondary=association_table)
-
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       back_populates="amenities")
     else:
         name = ""
