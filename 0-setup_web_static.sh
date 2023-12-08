bd6e8721a499b891b73182598aa6afd41ebcc3eb
koome <koomemc@gmail.com>
2023-12-07 16:32:59 -0800

    preparing web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-# script that sets up your web servers for the deployment of web_static
-apt-get -y update
-apt-get -y install nginx
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-echo "my name is Michael" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+# Bash script that sets up your web servers for the deployment of web_static
+
+
+source_folder="/data/web_static/releases/test"
+link_path="/data/web_static/current"
+ADD_WEBSTATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "Mic Cheque 1..2..3" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf "$source_folder" "$link_path"
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i "35i $ADD_WEBSTATIC" /etc/nginx/sites-available/default
 sudo service nginx restart
-exit 0
