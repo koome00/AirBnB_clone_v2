65aa35ea1476d5197981e1710ed31d787e6c2301
koome <koomemc@gmail.com>
2023-12-07 18:31:06 -0800

    full deployment

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+""""
+ -Fabric script (based on the file 1-pack_web_static.py) that distributes an
+  archive to your web servers, using the function deploy
+ -Prototype: def deploy():
+"""
+
+from fabric.api import local, env
+from time import strftime
+import os.path
+env.hosts = ["100.26.156.173", "54.157.184.250"]
+
+def do_pack():
+    """
+    generates a tar archive of webstatic
+    """
+    time_stamp = strftime("%Y%M%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        tar_file = "versions/web_static_{}.tgz".format(time_stamp)
+        local("tar -cvzf {} web_static".format(tar_file))
+        return tar_file
+    except:
+        return None
+
+def do_deploy(archive_path):
+    """
+    Deploy archive to web servers
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+    try:
+        filename = archive_path.split("/")[-1]
+        no_ext = filename.split(".")[0]
+        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
+        symlink = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+        run("rm -rf {}web_static".format(path_no_ext))
+        run("rm -rf {}".format(symlink))
+        run("ln -s {} {}".format(path_no_ext, symlink))
+        return True
+    except:
+        return False
+
+def deploy():
+    """
+    distributes archives to web servers
+    """
+    path = do_pack()
+    if path:
+        deployed = do_deploy(path)
+        return deployed
+    else:
+        return False
