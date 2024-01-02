7a626e39ef21bfa037be4b3e790558c7f63fc6b7
koome <koomemc@gmail.com>
2024-01-02 10:32:14 -0800

    rendering templaates

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
-Module 3
+Module 5
+Your web application must be listening on 0.0.0.0, port 5000
 Routes:
     /: display “Hello HBNB!”
     /hbnb: display “HBNB”
     /c/<text>: display “C ”, followed by the value of the text
     variable (replace underscore _ symbols with a space )
-    /python/<text>: display “Python ”, followed by the value of the text
-    variable (replace underscore _ symbols with a space )
-The default value of text is “is cool”
+    /python/(<text>): display “Python ”, followed by the value of
+    the text variable (replace underscore _ symbols with a space )
+    The default value of text is “is cool”
+    /number/<n>: display “n is a number” only if n is an integer
+    /number_template/<n>: display a HTML page only if n is an integer:
+        H1 tag: “Number: n” inside the tag BODY
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask("__name__")
+app = Flask(__name__, template_folder='../templates')
 
 
 @app.route("/", strict_slashes=False)
@@ -48,6 +52,11 @@ def display_python(text="is cool"):
 @app.route("/number/<int:n>", strict_slashes=False)
 def display_number(n):
     return f"{n:d} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_html(n):
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
