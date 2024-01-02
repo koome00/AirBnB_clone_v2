f9579a94ef7bce37aab81cbbd888a8f5afe5641a
koome <koomemc@gmail.com>
2024-01-02 21:38:14 +0300

    template rendering

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -54,10 +54,10 @@ def display_number(n):
     return f"{n:d} is a number"
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_html(n):
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def html_if_int(n):
     return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
