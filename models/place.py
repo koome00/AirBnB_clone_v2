fb68758f72bd54d5bc319d5762b8685898c53d83
koome <koomemc@gmail.com>
2024-01-04 23:03:52 +0300

    ewe

--- models/place.py
+++ models/place.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 import models
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
+
+place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey("places.id"),
                              primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
